4c8f5a12c8712837ea586959cac57ec477d90276
ozan göndür <ozangondur@outlook.com>
2025-12-01 09:55:55 +0300

    1

--- AITech.Business/Extensions/ServiceRegistrations.cs
+++ AITech.Business/Extensions/ServiceRegistrations.cs
@@ -1,7 +1,12 @@
 ﻿using AITech.Business.Services.AboutItemServices;
 using AITech.Business.Services.AboutServices;
+using AITech.Business.Services.BannerServices;
 using AITech.Business.Services.CategoryServices;
+using AITech.Business.Services.ChooseServices;
+using AITech.Business.Services.FAQServices;
+using AITech.Business.Services.FeatureServices;
 using AITech.Business.Services.ProjectServices;
+using AITech.Business.Services.SocialServices;
 using AITech.Business.Services.TestimonialServices;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -21,6 +26,11 @@ namespace AITech.Business.Extensions
             services.AddScoped<ITestimonialService, TestimonialService>();
             services.AddScoped<IAboutService, AboutService>();
             services.AddScoped<IAboutItemService, AboutItemService>();
+            services.AddScoped<IBannerService, BannerService>();
+            services.AddScoped<IChooseService, ChooseService>();
+            services.AddScoped<IFAQService, FAQService>();
+            services.AddScoped<IFeatureService, FeatureService>();
+            services.AddScoped<ISocialService, SocialService>();
         }
     }
 }
